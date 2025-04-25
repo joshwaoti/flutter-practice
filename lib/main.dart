d35b95b448619715650a63188b688f5ad1c70a0c
jotieno <jotieno@jambopay.co.ke>
2025-04-25 12:07:04 +0300

    created app that displays listview

--- lib/main.dart
+++ lib/main.dart
@@ -5,64 +5,32 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Dialog Demo', home: MyHomePage());
+    return MaterialApp(
+        title: 'Example App',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('List View'),
+            ),
+            body: ListView(children: [
+              ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Person One'),
+                  subtitle: Text('email'),
+                  trailing: Icon(Icons.arrow_forward),
+                  onTap: () => print('ListTile was Tapped')),
+              ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Person Two'),
+                  subtitle: Text('email'),
+                  trailing: Icon(Icons.arrow_forward),
+                  onTap: () => print('ListTile was Tapped')),
+              ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Person Three'),
+                  subtitle: Text('email'),
+                  trailing: Icon(Icons.arrow_forward),
+                  onTap: () => print('ListTile was Tapped'))
+            ])));
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _inputText = '';
-
-  void _showInputDialog() {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          TextEditingController controller = TextEditingController();
-          return AlertDialog(
-              title: Text("Enter some text"),
-              content: TextField(
-                controller: controller,
-                decoration: InputDecoration(hintText: 'Enter some Text'),
-              ),
-              actions: <Widget>[
-                TextButton(
-                  child: Text('Cancel'),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-                TextButton(
-                    child: Text('Save'),
-                    onPressed: () {
-                      setState(() {
-                        _inputText = controller.text;
-                      });
-                      Navigator.of(context).pop();
-                    })
-              ]);
-        });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Dialog Demo'),
-        ),
-        body: Center(
-            child: Text(
-          _inputText.isEmpty ? 'Type icon to enter text' : 'You entered: $_inputText',
-          style: TextStyle(fontSize: 20),
-        )
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _showInputDialog,
-          tooltip: 'Add a text',
-          child: Icon(Icons.edit),
-        ));
-  }
-}
+ 
